56cf3a26b568fccd95a35fd5fa49503dfa0c2a3b
ajdotguru <ajfsdeveloper@gmail.com>
2022-01-27 23:00:49 +0530

    rest_countries_app: add routing to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from 'components/sections';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const HomePage = lazy(() => import('components/pages/Home'));
 
-export default App;
+export const App = () => (
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route
+					index
+					element={
+						<Suspense fallback={<>...</>}>
+							<HomePage />
+						</Suspense>
+					}
+				/>
+			</Route>
+		</Routes>
+	</BrowserRouter>
+);
